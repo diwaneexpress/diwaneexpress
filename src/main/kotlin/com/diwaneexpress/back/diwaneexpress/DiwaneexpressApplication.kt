246a88420b327b4f76e2a760f2b753a1434e1331
Ababacar DIAW <diwaneexpress@gmail.com>
2021-08-08 17:57:29 +0000

    First Commit

---
+++ src/main/kotlin/com/diwaneexpress/back/diwaneexpress/DiwaneexpressApplication.kt
@@ -0,0 +1,11 @@
+package com.diwaneexpress.back.diwaneexpress
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiwaneexpressApplication
+
+fun main(args: Array<String>) {
+	runApplication<DiwaneexpressApplication>(*args)
+}
---
+++ src/test/kotlin/com/diwaneexpress/back/diwaneexpress/DiwaneexpressApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.diwaneexpress.back.diwaneexpress
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiwaneexpressApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
